6a5f99540791b90204a77c35b66657d1bb5073dc
TranDinhTuann <dtuan12122002@gmail.com>
2024-01-08 20:19:56 +0700

    Get API

--- src/App.js
+++ src/App.js
@@ -10,18 +10,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" width={'200px'} />
         <p>
-          Trần Đình Tuấn đây đây
+          Trần Đình Tuấn đến đây
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       <MyComponent></MyComponent>
       </header>
     </div>
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -2,27 +2,46 @@ import React from "react";
 
 class MyComponent extends React.Component {
     state = {
-        name: 'Huan',
-        age: '21'
+        firstName: '',
+        lastName: ''
     }
-    handleChangeName = (event) => {
+    handleChangeFirstName = (event) =>{
         this.setState({
-            name: event.target.value
+            firstName : event.target.value
         })
     }
+    handleChangeLastName = (event) =>{
+        this.setState({
+            lastName : event.target.value
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(">>> Check data input : ", this.state);
+    }
   render() {
+    console.log('>>> Call render: ', this.state);
     return (
-        <React.Fragment>
-            <div className="first">
-                <input value={this.state.name} type="text" onChange={(event) => this.handleChangeName(event)}></input>
-                My name is {this.state.name}
-            </div>
-            <div className="first">
-            My age is {this.state['age']}
-            </div>
-        </React.Fragment>
+        <div>
+            <form>
+                <label for="fname">First name:</label>
+                <input type="text" value={this.state.firstName}
+                    onChange={(event) => this.handleChangeFirstName(event)}
+                /><br/><br/>
+                <label for="lname">Last name:</label>
+                <input type="text" value={this.state.lastName}
+                    onChange={(event) => this.handleChangeLastName(event)}
+                /><br/><br/>
+                <input type="submit" value="Submit"
+                    onClick={(event) => this.handleSubmit(event)}
+                />
+            </form>
+        </div>
     )
   }
 }
 
 export default MyComponent;
+// git branch video9
+// git branch -l
+// git checkout video9
